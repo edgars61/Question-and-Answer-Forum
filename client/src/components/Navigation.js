150beacb5b74069a20661489b838482c3ce50d81
edgars61 <edgarsantana.62a@gmail.com>
2020-10-21 11:24:28 -0500

    added navigation bar

---
+++ client/src/components/Navigation.js
@@ -0,0 +1,56 @@
+import React, {Fragment} from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useRouteMatch,
+    useParams
+  } from "react-router-dom";
+
+
+import { Nav, Navbar, Form, FormControl } from 'react-bootstrap';
+import styled from 'styled-components';
+const Styles = styled.div`
+  .navbar { background-color: #222; }
+  a, .navbar-nav, .navbar-light .nav-link {
+    color: #9FFFCB;
+    &:hover { color: white; }
+  }
+  .navbar-brand {
+    font-size: 1.4em;
+    color: #9FFFCB;
+    &:hover { color: white; }
+  }
+  .form-center {
+    position: absolute !important;
+    left: 25%;
+    right: 25%;
+  }
+`;
+
+  function Navigation(){
+    
+    return (
+        <Fragment>
+            <Styles>
+            <Navbar expand="lg">
+      <Navbar.Brand href="/">Tutorial</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+      <Form className="form-center">
+        <FormControl type="text" placeholder="Search" className="" />
+      </Form>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item> 
+          <Nav.Item><Nav.Link href="/about">About</Nav.Link></Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+            </Styles>
+
+        </Fragment>
+    )
+  };
+
+ export default Navigation;
